7673353380c36616c2cc467a54dc27c31d3b6ee6
Zhang Henghua <1652709417@qq.com>
2025-07-18 17:06:43 +0800

    update

---
+++ nowcoder/noob8.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+	for {
+		n, _ := fmt.Scan(&a, &b)
+		if n == 0 {
+			break
+		} else {
+			fmt.Printf("%d %d\n", a/b, a%b)
+		}
+	}
+}

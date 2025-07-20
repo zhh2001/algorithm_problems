d445c98f5ff0f9bb714990641a6a33b9de0aa150
Zhang Henghua <1652709417@qq.com>
2025-07-20 23:32:50 +0800

    update

---
+++ nowcoder/noob64.go
@@ -0,0 +1,14 @@
+package main
+
+import "math"
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 求非负整数 n 的平方根
+ * @param n int整型 你需要求 n 的平方根
+ * @return double浮点型
+ */
+func findSqrt(n int) float64 {
+	return math.Sqrt(float64(n))
+}

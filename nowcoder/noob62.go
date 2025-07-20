ba9e67def2c9249a4f6633ad62cf75dee2390c2b
Zhang Henghua <1652709417@qq.com>
2025-07-20 23:27:06 +0800

    update

---
+++ nowcoder/noob62.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 实现求两个参数的和
+ * @param Integer1 int整型
+ * @param Integer2 int整型
+ * @return int整型
+ */
+func addTwoInteger(Integer1 int, Integer2 int) int {
+	return Integer1 + Integer2
+}

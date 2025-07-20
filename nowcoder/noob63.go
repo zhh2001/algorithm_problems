48339b60ae9a63b4e575b3a5486e725be2538a38
Zhang Henghua <1652709417@qq.com>
2025-07-20 23:28:40 +0800

    update

---
+++ nowcoder/noob63.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * 计算两个参数的乘积
+ * @param Number1 int整型
+ * @param Number2 int整型
+ * @return long长整型
+ */
+func aTimesB(Number1 int, Number2 int) int64 {
+	return int64(Number1) * int64(Number2)
+}
